bd633d594ec8321fc79f919e7f03a5a4b7bef574
Radmir <radmirka11@gmail.com>
2022-06-13 23:53:10 +0300

    adding files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 10_000;
+        int m = 20;
+
+        int bonus = cost / m;
+        System.out.println(bonus);
+    }
+}
